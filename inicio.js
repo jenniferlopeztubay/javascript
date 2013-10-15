d2fcfca6c6ddb5c53166cae0b90eade40bf442db
jennifer <jennmilt25@hotmail.com>
2013-10-14 21:57:39 -0500

    Update inicio.js

--- inicio.js
+++ inicio.js
@@ -1,2 +1,92 @@
 javascript
 ==========
+<script type="text/javascript">
+function writeHTMLasJS(){
+document.write("");
+document.write("<!DOCTYPE HTML>");
+document.write("<head>");
+document.write("	<meta http-equiv=\"content-type\" content=\"text\/html\" \/>");
+document.write("	<meta name=\"author\" content=\"\" \/>");
+document.write("");
+document.write("	<title>MEGAFRIO<\/title>");
+document.write(" <b><l>");
+document.write("<CENTER>");
+document.write("<font color=\"navy\" face=\"Batang\" size=\"3\">");
+document.write("<IMG SRC=\"megafrio.png\" width=\"400\" height=\"100\" >");
+document.write("<BODY bgcolor=\"#1C597A\">");
+document.write("<\/CENTER>");
+document.write("<hr \/>");
+document.write("<marquee direction=\"right\" behavior=\"alternate\" bgcolor=\"#497F9C\"><font color=\"aqua\"> EFICIENCIA Y CALIDAD");
+document.write("<\/marquee> ");
+document.write("<\/b><\/l>");
+document.write("");
+document.write("<font color=\"black\" face=\"Batang\" size=\"2\">");
+document.write("<Center><H4>Direccion: Aurora 2 MzF V31   TELF: 0994044518 - 0984343020<\/H4><\/Center>");
+document.write("<CENTER><H4>MANTA - Manabi - Ecuador<\/H4><\/CENTER>");
+document.write("");
+document.write("<\/head>");
+document.write("");
+document.write("");
+document.write("");
+document.write("<meta http-equiv=\"Content-Type\" content=\"text\/html; charset=utf-8\" \/>");
+document.write("<link rel=\"stylesheet\" href=\"cssmenu.css\" type=\"text\/css\" \/>");
+document.write("");
+document.write("<body >");
+document.write("<div id=\"cssmenu\">");
+document.write("	<ul class=\"nav\">");
+document.write("    ");
+document.write("<\/div>");
+document.write("<div id='cssmenu'>");
+document.write("<ul>");
+document.write("   <li><a href='index.html'><span>Home<\/span><\/a><\/li>");
+document.write("   <li class='has-sub'><a href='#'><span>Repuestos y materiales<\/span><\/a>");
+document.write("      <ul>");
+document.write("         <li class='has-sub'><a href='#'><span>Gases Refrigerantes<\/span><\/a>");
+document.write("         <li class='has-sub'><a href='#'><span>Compresores<\/span><\/a>");
+document.write("            <ul>");
+document.write("               <li><a href='#'><span>Compresores Hermeticos<\/span><\/a><\/li>");
+document.write("               <li class='last'><a href='#'><span>Compresores semi-hermeticos<\/span><\/a><\/li>");
+document.write("            <\/ul>");
+document.write("         <\/li>");
+document.write("         <li class='has-sub'><a href='#'><span>Herramientas en general<\/span><\/a>");
+document.write("            <ul>");
+document.write("               <li><a href='#'><span>Tuberias de cobre<\/span><\/a><\/li>");
+document.write("               <li class='last'><a href='#'><span>Soldadura de plata<\/span><\/a><\/li>");
+document.write("            <\/ul>");
+document.write("         <\/li>");
+document.write("      <\/ul>");
+document.write("   <\/li>");
+document.write("   <li class='has-sub'><a href='#'><span>Aires Acondicionados<\/span><\/a>");
+document.write("      <ul>");
+document.write("         <li class='has-sub'><a href='#'><span>Tipo Ventana<\/span><\/a>");
+document.write("         <\/li>");
+document.write("         <li class='has-sub'><a href='#'><span>Tipo Paquete<\/span><\/a>");
+document.write("         <\/li>");
+document.write("         <li class='has-sub'><a href='#'><span>Tipo Piso techo<\/span><\/a>");
+document.write("         <\/li>");
+document.write("         <li class='has-sub'><a href='#'><span>Tipo Split<\/span><\/a>");
+document.write("            <ul>");
+document.write("               <li><a href='#'><span>Multi Split<\/span><\/a><\/li>");
+document.write("               <li class='last'><a href='#'><span>Split Ducto<\/span><\/a><\/li>");
+document.write("            <\/ul>");
+document.write("         <\/li>");
+document.write("      <\/ul>");
+document.write("   <\/li>");
+document.write("   <li class='last'><a href='#'><span>Contactenos<\/span><\/a><\/li>");
+document.write("<\/ul>");
+document.write("<\/div>");
+document.write("<img src=\"aires.gif\"  align=\"right\" width=\"534\" height=\"350\" border=\"5\" >");
+document.write("<BODY bgcolor=\"#1C597A\">");
+document.write("");
+document.write("");
+document.write("<\/body>");
+document.write("<\/html>");
+document.write("");
+}
+</script>
+
+ 
+
+<script type="text/javascript">
+writeHTMLasJS();
+</script>
